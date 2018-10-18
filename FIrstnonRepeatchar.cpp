596d7ee8e4a23542ebeedb56d37cff16cc157ea2
Sreejith S <32271222+sreejithsankar55@users.noreply.github.com>
2018-10-18 14:09:15 +0530

    Create FIrstnonRepeatchar.cpp

---
+++ FIrstnonRepeatchar.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define ios ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+using namespace std;
+int main()
+{	
+	ios;
+	string s;
+	cin>>s;
+	ll si = s.size();
+	if(si==1)
+		return cout<<s,0;
+	/*--------2 pointer approach -----*/
+	ll i=0,j=1;
+	while(j<si)
+	{
+		if(s[i]==s[j])
+			i++;
+		j++;
+	}
+	cout<<s[i]<<endl;
+	
+	/*--------Using Maps---------------------*/
+	
+	map < char,ll > m;
+	for(ll i = 0 ; i < si ; i++)
+	{
+		if(m.find(s[i])!=m.end())
+			m[s[i]]--;
+		else
+			m[s[i]]++;
+	}
+	for(ll i = 0 ; i < si ; i++)
+	{
+		if(m[s[i]]>0)
+			return cout<<s[i],0;
+	}
+	
+    return 0;     
+ } 	   
