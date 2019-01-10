f23845c3f6956c521fa0ac3d12d2027531683c25
Sreejith S <32271222+sreejithsankar55@users.noreply.github.com>
2019-01-10 21:28:45 +0530

    Update Extract_2_non_repeat_no.cpp

--- Extract_2_non_repeat_no.cpp
+++ Extract_2_non_repeat_no.cpp
@@ -6,7 +6,7 @@ Your algorithm should run in linear runtime complexity. Could you implement it u
 
 https://leetcode.com/problems/single-number-iii/
 */
-
+//Explanation for custom input
 class Solution {
 public:
     vector<int> singleNumber(vector<int>& nums) 
@@ -14,11 +14,12 @@ public:
         int s=0;
         vector < int > v(2,0);
         for(int i = 0 ; i < (int)nums.size() ; i++)
-            s^=nums[i];
-        int c=s&-s;
+            s^=nums[i];         //Taking xor of all 3^5 remains
+        int c=s&-s; //Set bit from right here value is here 1 ( if s=12 then c is 4)
+        // If we get that set bit we can make sure that one of the numbers bit at that position is 0 and the other is 1
         for(int i = 0 ; i < (int)nums.size() ; i++)
         {
-            if(nums[i]&c)
+            if(nums[i]&c)   // If this turns to be true that is there exists any set bit it puts to if part else ,moves to else part
                 v[0]^=nums[i];
             else
                 v[1]^=nums[i];
@@ -27,3 +28,4 @@ public:
     }
    
 };
+// Also available in geeksforgeeks
